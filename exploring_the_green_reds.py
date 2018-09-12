e1dc1a28d5630bea68d8885288916e3ceac9b6e3
Dima Zui <dimka.zui98@gmail.com>
2018-09-12 16:30:55 +0300

    exploring green red dots done

--- exploring_the_green_reds.py
+++ exploring_the_green_reds.py
@@ -1,92 +1,77 @@
 #!/usr/bin/env python3
 
+# source activate my_env
+
 import pandas as pd
+import random
+import itertools
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-# read data from a 'csv' file
-# parse it and return as a dictionary
-def read_csv(file_name):
-	# read a content of the file and split into lines
-	file_data = open(file_name).read().split('\n')
-	# first line will hold the keys 
-	# remove all the quotes from the string
-	file_data[0] = file_data[0].replace('"', '')
-	# get list of keys
-	file_data_keys = file_data[0].split(';')
-	# remove first string after it was processed
-	file_data.pop(0)
-	# extract all the data
-	values = []
-	for line in file_data:
-		# split the line to get list of numbers
-		line_split = line.split(';')
-		to_add = []
-		# go through each of them and add to the resulting list, convert to float
-		for line_part in line_split:
-			if line_part != "":
-				to_add.append(float(line_part))
-		# append it to the values list
-		values.append(to_add)
-	# create a dictionary, that will hold the values for each parameter
-	data_set = {key: [] for key in file_data_keys}
-	j = 0
-	cols = len(data_set.items())
-	for key in data_set.keys():
-		col = []
-		for elem in values:
-			col.append(elem[j])
-		j += 1
-		data_set[key] = col
-	return (data_set)
-
+from pandas.plotting import scatter_matrix
 
 # function to print a graph basing on the data
-def plot_graph(data, good_threshold, bad_threshold):
+
+def plot_graph(data, good_threshold, bad_threshold, save_plot=False):
+
+	# get the number of variables to process and get know the size of scatter matrix
+	numvars = len(data.keys())
+	# 
+	# create an figure an subplots to wotk with
+	fig, axes = plt.subplots(nrows=numvars, ncols=numvars, figsize=(25,25))
+
+	# Hide all ticks and labels for the plots on the matrix
+	# where row_nb == column_nb, those are labels for the parameter
+	for i in range(numvars):
+		for j in range (numvars):
+			axes[i, j].xaxis.set_visible(False)
+			axes[i, j].yaxis.set_visible(False)
 	
-	# demo to plot two random graphs on the same line
+	# # Label the diagonal subplots, taking keys of the dict as a text for them
+	for i, label in enumerate(data.keys()):
+		axes[i, i].annotate(label.capitalize(), (0.5, 0.5), xycoords='axes fraction', ha='center', va='center')
 
-	fig = plt.figure('Test plots')
+	good_wine = data[data['quality'] > good_threshold]
+	bad_wine = data[data['quality'] < bad_threshold]
 
-	# first two number represent the size of imaginary grid
-	# second its position
-	# example of plot's indexes
-	# 1 2
-	# 3 4
-	plot_1 = fig.add_subplot(221)
-	plot_1.plot([(1, 2), (3, 4)], [(4, 3), (2, 3)])
-
-	plot_2 = fig.add_subplot(222)
-	plot_2.plot([(7, 2), (5, 3)], [(1, 6), (9, 5)])
-
-	# print data I work with
-	print(data['fixed acidity'])
-	print(data['volatile acidity'])
-	
-	# specify range of x and y axis
-	max_x = max(data['fixed acidity'])
-	max_x +=  max_x * 0.15
-	max_y = max(data['volatile acidity'])
-	max_y += max_y * 0.15
-	
-	# plot the graph
-	plot_3 = fig.add_subplot(223)
-	plot_3.plot(data['fixed acidity'], data['volatile acidity'], 'ro')
-	plot_3.axis([0, max_x, 0, max_y])
+	i = 0
+	for key_1 in good_wine:
+		j = 0;
+		for key_2 in good_wine:
+			if key_1 != key_2:
+				axes[i, j].plot(good_wine[key_2], good_wine[key_1], 'g.')
+			j += 1
+		i += 1
 
 
+	i = 0
+	for key_1 in bad_wine:
+		j = 0;
+		for key_2 in bad_wine:
+			if key_1 != key_2:
+				axes[i, j].plot(bad_wine[key_2], bad_wine[key_1], 'r.')
+			j += 1
+		i += 1
+
+
+	# # to add spacing between all the plots
+	fig.tight_layout()
+
+	# # display all the plots
 	plt.show()
+
+	if save_plot:
+		fig.savefig('whine-quality-red-scatter-matrix.png')
 
 
 # a main function to run the demo
 def main():
-	# read_csv('winequality-red.csv')
-	data_set = read_csv('test.csv')
-	plot_graph(data_set, 0, 0)
+	
+	df = pd.read_csv("winequality-red.csv", sep=';')
+
+	plot_graph(df, 6, 5, True)
 	return
 
 
 if __name__ == '__main__':
 	main()
-	pass
+	
